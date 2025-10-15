dd08a430028784e997dc3a3e2ba882923af21978
kevin <onedayatatime0923@gmail.com>
2025-10-15 16:59:23 -0700

    kevin

--- src/test.py
+++ src/test.py
@@ -8,5 +8,5 @@ env = gym.make('MergeEnv-v0', render_mode='human')
 obs, info = env.reset()
 done = truncated = False
 while not (done or truncated):
-    action = np.array([1,1]) # Your agent code here
+    action = np.array([0,0]) # Your agent code here
     obs, reward, done, truncated, info = env.step(action)
