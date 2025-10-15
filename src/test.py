553ded6f5108a813784f4d9ae6c23476d5aa97e4
kevin <onedayatatime0923@gmail.com>
2025-10-15 16:58:20 -0700

    kevin

---
+++ src/test.py
@@ -0,0 +1,12 @@
+import numpy as np
+import gymnasium as gym
+import bluesky_gym
+bluesky_gym.register_envs()
+
+env = gym.make('MergeEnv-v0', render_mode='human')
+
+obs, info = env.reset()
+done = truncated = False
+while not (done or truncated):
+    action = np.array([1,1]) # Your agent code here
+    obs, reward, done, truncated, info = env.step(action)
