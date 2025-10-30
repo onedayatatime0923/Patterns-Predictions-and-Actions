7d6a67e0811861ebc61cd61dad9cb64e187a8e3c
kevin <onedayatatime0923@gmail.com>
2025-10-30 16:37:53 -0700

    kevin

--- test.py
+++ test.py
@@ -5,7 +5,11 @@ for g in games:
     print(g)
     #  print(g.__dict__)
     players = nflgame.combine_game_stats([g])
-    print(players)
+    for p in players.rushing().sort('rushing_yds'):
+        msg = '%s %d carries for %d yards and %d TDs'
+        print(msg % (p, p.rushing_att, p.rushing_yds, p.rushing_tds))
+    input()
+    #  print(players)
 
 #  players = nflgame.combine_game_stats(games)
 #  for p in players.rushing().sort('rushing_yds').limit(5):
