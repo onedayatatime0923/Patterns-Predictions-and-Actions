fabe7dc77d5700808bd94ef579dc3c8351afd6c8
kevin <onedayatatime0923@gmail.com>
2025-10-30 14:21:59 -0700

    kevin

---
+++ test.py
@@ -0,0 +1,13 @@
+import nflgame
+
+games = nflgame.games(2019, week=1)
+for g in games:
+    print(g)
+    #  print(g.__dict__)
+    players = nflgame.combine_game_stats([g])
+    print(players)
+
+#  players = nflgame.combine_game_stats(games)
+#  for p in players.rushing().sort('rushing_yds').limit(5):
+#      msg = '%s %d carries for %d yards and %d TDs'
+#      print(msg % (p, p.rushing_att, p.rushing_yds, p.rushing_tds))
